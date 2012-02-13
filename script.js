2ba8356b70e8bd4015037ad2970a861c3170a6d4
Nikki Lee <nikki@nicoleblee.com>
2012-02-12 22:50:28 -0800

    JSON debugged, thanks to @ericyhwang's awesomeness

--- script.js
+++ script.js
@@ -9,56 +9,57 @@ $(document).ready(function() {
 
 	$.getJSON("http://spreadsheets.google.com/feeds/list/0ArWU2T0HEMrldGxLeHVFYmM2VVhvMktFRVJwVGtVOHc/od6/public/values?alt=json-in-script&callback=?",
 		function(data) {
-		  for (i=0;i<data.feed.entry.length;i++) {
-		    var entry = data.feed.entry[i];
-		    var chapter = {
-		      name: entry["gsx$name"].$t,
-		      country: entry["gsx$country"].$t,
-		      founding: parseInt(entry["gsx$founding"].$t),
-		      women: parseInt(entry["gsx$women"].$t),
-		      men: parseInt(entry["gsx$men"].$t),
-		      grants: parseInt(entry["gsx$grants"].$t),
-		      dollars: parseInt(entry["gsx$dollars"].$t)
-		    };
+			for (i=0;i<data.feed.entry.length;i++) {
+			    var entry = data.feed.entry[i];
+			    var chapter = {
+			      name: entry["gsx$name"].$t,
+			      country: entry["gsx$country"].$t,
+			      founding: parseInt(entry["gsx$founding"].$t),
+			      women: parseInt(entry["gsx$women"].$t),
+			      men: parseInt(entry["gsx$men"].$t),
+			      grants: parseInt(entry["gsx$grants"].$t),
+			      dollars: parseInt(entry["gsx$dollars"].$t)
+			    };
 
-		    chapter_list.push(chapter);
-		  }
+			    chapter_list.push(chapter);
+			}
+
+			displayData();
 	});
 
-    console.log(chapter_list[1]);
+	var displayData = function() {
+		// create our canvas
+		// params: x, y, width, height
+		var paper = new Raphael(document.getElementById("canvas"), $("#canvas").width(), $("#canvas").height());
 
-	// create our canvas
-	// params: x, y, width, height
-	var paper = new Raphael(document.getElementById("canvas"), $("#canvas").width(), $("#canvas").height());
+		console.log(chapter_list[0]["women"]);
+		console.log(chapter_list[2]["women"]);
+		console.log(chapter_list[0]["men"]);
+		console.log(chapter_list[2]["men"]);
 
-	console.log(chapter_list[0]["women"]);
-	console.log(chapter_list[2]["women"]);
-	console.log(chapter_list[0]["men"]);
-	console.log(chapter_list[2]["men"]);
+		// create a stacked bar chart of our data
+		// params: x, y, width, height, [ [values1], [values2] ]
+		chart = paper.hbarchart(100,100,200,100,[ [chapter_list[0]["women"], chapter_list[2]["women"]], [chapter_list[0]["men"], chapter_list[2]["men"]] ], {stacked: true});
 
-	// create a stacked bar chart of our data
-	// params: x, y, width, height, [ [values1], [values2] ]
-	chart = paper.hbarchart(100,100,200,100,[ [chapter_list[0]["women"], chapter_list[2]["women"]], [chapter_list[0]["men"], chapter_list[2]["men"]] ], {stacked: true});
+		// create a hover function
+		chart.hover(
+			function() {
+				console.log(this);
+				// create a popup element on top of the bar
+		        this.flag = paper.popup(this.bar.x, this.bar.y-10, (this.bar.value || "0") + " people").insertBefore(this);
+			    },
+			function() {
+			    // hide the popup element with an animation and remove the popup element at the end
+			    this.flag.animate({opacity: 0}, 300, function () {this.remove();});
+			}
+		);
 
-	// create a hover function
-	chart.hover(
-		function() {
-			console.log(this);
-			// create a popup element on top of the bar
-	        this.flag = paper.popup(this.bar.x, this.bar.y-10, (this.bar.value || "0") + " people").insertBefore(this);
-		    },
-		function() {
-		    // hide the popup element with an animation and remove the popup element at the end
-		    this.flag.animate({opacity: 0}, 300, function () {this.remove();});
-		}
-	);
-
-	// add tooltips
-	$("#canvas a").qtip({
-		style: {
-			tip: true,
-			classes: 'ui-tooltip-tipsy ui-tooltip-shadow'
-		}
-	});
-
+		// add tooltips
+		$("#canvas a").qtip({
+			style: {
+				tip: true,
+				classes: 'ui-tooltip-tipsy ui-tooltip-shadow'
+			}
+		});
+	}
 });
